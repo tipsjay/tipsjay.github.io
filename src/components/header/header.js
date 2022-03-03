5f78964d58ba2db8fc432d6aa188865ea37e563c
jiglisia <jiglisia@dnamicro.com>
2022-03-03 13:41:30 +0800

    Create: created version 1 of portfolio && deploy to github pages

---
+++ src/components/header/header.js
@@ -0,0 +1,63 @@
+
+
+import React,{Component} from 'react';
+
+export default class header extends Component {
+  render() {
+    return (
+       <React.Fragment>
+
+      <header className="s-header">
+        <div className="row s-header__nav-wrap">
+          <nav className="s-header__nav">
+            <ul>
+              <li className="current"><a className="smoothscroll" href="#hero">Home</a></li>
+              <li><a className="smoothscroll" href="#about">About</a></li>
+              <li><a className="smoothscroll" href="#resume">Resume</a></li>
+              <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+              <li><a className="smoothscroll" href="#contact">Say Hello</a></li>
+            </ul>
+          </nav>
+        </div> {/* end row */}
+        <a className="s-header__menu-toggle" href="#0" title="Menu">
+          <span className="s-header__menu-icon" />
+        </a>
+      </header>
+
+      <section id="hero" class="s-hero target-section">
+
+      <div class="s-hero__bg rellax" data-rellax-speed="-7"></div>
+
+      <div class="row s-hero__content">
+          <div class="column">
+
+              <div class="s-hero__content-about">
+
+                  <h1>I'm Jonathan Doe.</h1>
+
+                  <h3>
+                  I'm a Manila based <span>graphic designer</span>, <span>illustrator</span> and <span>webdesigner</span> creating awesome and
+                  effective visual identities for companies of all sizes around the globe. Let's <a class="smoothscroll" href="#about">start scrolling</a>
+                  and learn more <a class="smoothscroll" href="#about">about me</a>.
+                  </h3>
+
+                  <div class="s-hero__content-social">
+                      <a href="#0"><i class="fab fa-facebook-square" aria-hidden="true"></i></a>
+                      <a href="#0"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+                      <a href="#0"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+                      <a href="#0"><i class="fab fa-dribbble" aria-hidden="true"></i></a>
+                      <a href="#0"><i class="fab fa-behance" aria-hidden="true"></i></a>
+                      <a href="#0"><i class="fab fa-linkedin" aria-hidden="true"></i></a>
+                  </div>
+
+              </div>
+          </div>
+      </div>
+   
+      </section>
+   
+);
+       </React.Fragment>
+    )
+  }
+}
